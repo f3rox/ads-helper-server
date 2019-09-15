7e90b894289cf7ab7945c8eda3e6bc8da8b1c4f2
iodkowski <idkwsk@gmail.com>
2019-09-15 16:58:00 +0300

    FutureEither implemented

---
+++ app/monads/FutureEither.scala
@@ -0,0 +1,14 @@
+package monads
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+case class FutureEither[A](value: Future[Either[Throwable, A]]) {
+  def map[B](f: A => B): FutureEither[B] = FutureEither(value.map(_.map(f)))
+
+  def flatMap[B](f: A => FutureEither[B]) =
+    FutureEither(value.flatMap {
+      case Right(eitherValue) => f(eitherValue).value
+      case Left(exception) => Future.failed(exception)
+    })
+}
